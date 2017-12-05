537f18545509dde61c7777458d6d88477a1f1851
Salvador Sastre <chavosastre@gmai.com>
2017-12-05 16:11:01 -0600

    Creación de Vistas

--- HolaMundo/Controllers/HomeController.cs
+++ HolaMundo/Controllers/HomeController.cs
@@ -19,22 +19,25 @@ namespace HolaMundo.Controllers
         {
             ViewBag.Message = "Your application description page.";
 
-            var peliculasService = new PeliculasService();
-            var model = peliculasService.ObtenerPeliculas();
+            //var peliculasService = new PeliculasService();
+            //var model = peliculasService.ObtenerPeliculas();
 
-            return View(model);
+            return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-            ViewBag.UnInt = 45;
-            ViewBag.UnaFecha = new DateTime(1800,4,7);
-            ViewData["MiMensaje"] = "Esto fué creado con ViewData";
+            //ViewBag.UnInt = 45;
+            //ViewBag.UnaFecha = new DateTime(1800,4,7);
+            //ViewData["MiMensaje"] = "Esto fué creado con ViewData";
 
             return View();
         }
 
-
+        public ActionResult MiAction()
+        {
+            return View();
+        }
     }
 }

43c5c1082d0104ba2587364b8af54104720290b4
Salvador Sastre <chavosastre@gmai.com>
2018-01-18 18:11:19 -0600

    26.-Modelos, Scaffolding y Entity Framework   Modelos

--- HolaMundo/Models/Pelicula.cs
+++ HolaMundo/Models/Pelicula.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace HolaMundo.Models
 {
     public class Pelicula
     {
+        public int Id { get; set; }
+        [Required]
         public string Titulo { get; set; }
         public bool EstaEncartelera { get; set; }
+        [StringLength(120)]
         public string Genero { get; set; }
     }
 } 
